c6d1f97f0609f4d1007b3cd3460f6f4009a3daa5
ma9yam <ma9yam@gmail.com>
2021-04-04 23:54:12 +1000

    Dialogue system added

---
+++ Tech demo/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("error" + gameObject.name);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject dialogueBox;
+    public Text dialogueName;
+    public Text dialogueText;
+    public Image dialoguePortrait;
+    public float delay = 0.001f;
+
+    public Queue<DialogueBase.Info> dialogueInfo = new Queue<DialogueBase.Info>();
+
+    public void EnqueueDialogue (DialogueBase db)
+    {
+        dialogueBox.SetActive(true);
+        dialogueInfo.Clear();
+        foreach (DialogueBase.Info info in db.dialogueinfo)
+        {
+            dialogueInfo.Enqueue(info);
+        }
+        DequeueDialogue();
+    }
+
+    public void DequeueDialogue()
+    {
+        if (dialogueInfo.Count == 0)
+        {
+            endOfDialogue();
+            return;
+        }
+        DialogueBase.Info info = dialogueInfo.Dequeue();
+        dialogueName.text = info.myName;
+        dialogueText.text = info.myText;
+        dialoguePortrait.sprite = info.portrait;
+        StartCoroutine(TypeText(info));
+    }
+
+    IEnumerator TypeText(DialogueBase.Info info)
+    {
+        dialogueText.text = "";
+        foreach (char c in info.myText.ToCharArray())
+        {
+            yield return new WaitForSeconds(delay);
+            dialogueText.text += c;
+            yield return null;
+        }
+    }
+    public void endOfDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+}
---
+++ Tech demo/Assets/Scripts/Dialogue Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public DialogueBase dialogue;
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.EnqueueDialogue(dialogue);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter pressed");
+            TriggerDialogue();
+        }
+    }
+}

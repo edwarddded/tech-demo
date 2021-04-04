4eae54bf768bc3d3e89b2bcbdb2af93773c488bc
kephen <kevin.nguyen2511@gmail.com>
2021-04-04 17:36:16 +1000

    2nd skill added to showcase mechanics (Tech Demo)

---
+++ Tech demo/Assets/Scripts/Soul Skills/Combust.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combust : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
+}
--- Tech demo/Assets/Scripts/Soul Skills/PlayerSkillDictionary.cs
+++ Tech demo/Assets/Scripts/Soul Skills/PlayerSkillDictionary.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerSkillDictionary : MonoBehaviour
 {
     public Transform firepoint;
+
     public GameObject iceShotPrefab;
+    public GameObject combustPrefab;
 
     void Start()
     {
@@ -22,7 +24,7 @@ public class PlayerSkillDictionary : MonoBehaviour
         if (abilityIndex == 1)
             IceShot();
         if (abilityIndex == 2)
-            Test2();
+            Combust();
     }
 
     //Below is the collection of all the player skills
@@ -35,9 +37,24 @@ public class PlayerSkillDictionary : MonoBehaviour
     }
 
     // #2 
-    void Test2()
+    void Combust()
     {
         Debug.Log("Ability 2 Activated");
-    }
 
+        Instantiate(combustPrefab, this.transform.position, this.transform.rotation);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(this.transform.position, 2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].CompareTag("Enemy"))
+            {
+                Enemy enemy = colliders[i].gameObject.GetComponent<Enemy>();
+                if(enemy != null)
+                {
+                    enemy.TakeDamage(3f);
+                }
+            }
+        }
+       
+    }
 }

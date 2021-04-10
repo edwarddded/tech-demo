66b813838ad4d4c020ff11a3ce28e0c9e22d5250
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-04-10 13:41:45 +1000

    fix jump, and movement

--- Tech demo/Assets/Scripts/PlayerController.cs
+++ Tech demo/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float JumpForce = 1f;
-    float moveSpeed = 8f;
+    public float JumpForce = 1f;
+    public float moveSpeed = 20f;
     //public bool isGrounded = false;
     private Rigidbody2D rb;
     private Animator anim;
+
+    private bool IsGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float JumpTimeCounter;
+    public float jumtime;
+    private bool isjumping;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +29,51 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Item")
         {
-            
                 Destroy(collision.gameObject);
-           
         }
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movement
+        float direction = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveSpeed * direction * Time.fixedDeltaTime, rb.velocity.y);
+        if (direction != 0f)
+        {
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * direction, transform.localScale.y);
+        }
+
+        //if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        //{
+        //    rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        //}
+    }
     void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime * moveSpeed;
-
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        IsGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (IsGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            isjumping = true;
+            JumpTimeCounter = jumtime;
+            rb.velocity = Vector2.up * JumpForce;
         }
+        if (Input.GetKey(KeyCode.Space) && isjumping == true)
+        {
+            if (JumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * JumpForce;
+                JumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isjumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isjumping = false;
+        }
+
     }
 }

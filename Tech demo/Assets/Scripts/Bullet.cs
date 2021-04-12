40fc3623b3b8d018b4920ca1f0f0cd57626be5e9
kephen <kevin.nguyen2511@gmail.com>
2021-04-12 14:05:39 +1000

    Fixed player firing in the wrong direction when flipped

--- Tech demo/Assets/Scripts/Bullet.cs
+++ Tech demo/Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
     public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
--- Tech demo/Assets/Scripts/PlayerController.cs
+++ Tech demo/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     //public bool isGrounded = false;
     private Rigidbody2D rb;
     private Animator anim;
+    private GameObject fp;
 
     private bool IsGrounded;
     public Transform feetPos;
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        fp = transform.Find("firePoint").gameObject;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -42,7 +44,13 @@ public class PlayerController : MonoBehaviour
         if (direction != 0f)
         {
             transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * direction, transform.localScale.y);
+
+            if (transform.localScale.x == -1)
+                fp.transform.rotation = Quaternion.Euler(0, -180, 0);
+            else
+                fp.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+
         
     }
     void Update()

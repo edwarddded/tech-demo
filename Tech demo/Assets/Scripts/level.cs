fb076d926ff0623c8b9cc80a108bcc8a36b97e8f
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2021-04-17 23:41:31 +1000

    start scene

---
+++ Tech demo/Assets/Scripts/level.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Load()
+    {
+        int level = Random.Range(1, 4);
+        if (level == 1)
+        {
+            SceneManager.LoadSceneAsync(1);
+           
+        }
+        else if (level == 2)
+        {
+            SceneManager.LoadSceneAsync(2);
+          
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(3);
+           
+        }
+    }
+}
+

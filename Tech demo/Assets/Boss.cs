b6c5fd4daf01fd4f1d9e0f92a31394d19bfca37f
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-04-24 16:38:36 +1000

    US::013- Change Boss attack animation and set up the time when the boss will shoot laser (shoot laser once each 8s)
    US::013- Change Boss attack animation and set up the time when the boss will shoot laser (shoot laser once each 8s)

---
+++ Tech demo/Assets/Boss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private float time = 8f;
+    public Animator Ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time - Time.deltaTime;
+        Debug.Log(time);
+        if (time < 0)
+        {
+            time = 8f;
+        }
+        if (time <2.5)
+        {
+            Ani.SetBool("IsAttack", true);
+        }
+        else
+        {
+            Ani.SetBool("IsAttack", false);
+        }
+        
+    }
+}

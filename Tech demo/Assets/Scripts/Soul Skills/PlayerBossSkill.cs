f7ff6419fc8490a835e1e9be7ee1f34d495ca6fc
kephen <kevin.nguyen2511@gmail.com>
2021-05-04 16:30:00 +1000

    US:033 - Boss ability keybound with working cooldown functionality, cooldown visible on HUD too (No actual ability implemented yet)

---
+++ Tech demo/Assets/Scripts/Soul Skills/PlayerBossSkill.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBossSkill : MonoBehaviour
+{
+    public Image imageCooldown;
+
+    private bool isCooldown = false;
+    private float cooldownDuration = 15f;
+    private float cooldownTimer;
+
+    void Start()
+    {
+        imageCooldown.fillAmount = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            UseAbility();
+        }
+
+        if (isCooldown)
+        {
+            ApplyCooldown();
+        }
+    }
+
+    void ApplyCooldown()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        if(cooldownTimer <= 0.0f)
+        {
+            isCooldown = false;
+            imageCooldown.fillAmount = 0.0f;
+        }
+        else
+        {
+            imageCooldown.fillAmount = cooldownTimer / cooldownDuration;
+        }
+    }
+
+    public void UseAbility()
+    {
+        if (isCooldown)
+        {
+            
+        }
+        else
+        {
+            isCooldown = true;
+            cooldownTimer = cooldownDuration;
+
+            // Insert boss skill here
+
+        }
+    }
+}

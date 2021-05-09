85d214397ed67ebe775b9e26af3fff1827a56601
kephen <kevin.nguyen2511@gmail.com>
2021-05-09 16:08:03 +1000

    [US:031 - I want the ability of enemies of level 1 enemies to be stronger] - Completely new ability added to the game dropped by the goblin - player throws 8 daggers around themselves

---
+++ Tech demo/Assets/Scripts/Soul Skills/GoblinDaggerBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinDaggerBehaviour : MonoBehaviour
+{
+    public float speed = 10f;
+    public float damage = 5f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Enemy enemy = col.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+        BossPart bossPart = col.GetComponent<BossPart>();
+        if (bossPart != null)
+        {
+            bossPart.TakeDamage(damage);
+            Destroy(gameObject);
+
+        }
+
+        Boss boss = col.GetComponent<Boss>();
+        if (boss != null)
+        {
+            boss.TakeDamage(damage);
+            Destroy(gameObject);
+
+        }
+
+    }
+}

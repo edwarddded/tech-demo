187926f59e79ec4fa4ef07c72b486534532259ef
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2021-05-11 16:36:58 +1000

    US:050 As a developer, i want there are ice tracks in the ice level.

---
+++ Tech demo/Assets/Scripts/IceTrack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTrack : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x - Player.transform.position.x < 5 || this.transform.position.y < 12)
+        {
+            transform.Translate(0, -10 * Time.deltaTime, 0, Space.Self);
+        }
+    }
+}

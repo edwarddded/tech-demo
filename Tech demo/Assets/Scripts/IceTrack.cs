a92ad255bcd918f570fc3292440a97d5e8d46a6e
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2021-05-16 18:43:36 +1000

    US::015 - As a developer, I want the level design have different theme. (Level 3 Layout)

--- Tech demo/Assets/Scripts/IceTrack.cs
+++ Tech demo/Assets/Scripts/IceTrack.cs
@@ -17,7 +17,7 @@ public class IceTrack : MonoBehaviour
     {
         if (Player != null)
         {
-            if (gameObject.transform.position.x - Player.transform.position.x <= 5)
+            if (gameObject.transform.position.x - Player.transform.position.x <= 5 || this.transform.position.y < 9)
             {
                 transform.Translate(0, -10 * Time.deltaTime, 0, Space.Self);
             }

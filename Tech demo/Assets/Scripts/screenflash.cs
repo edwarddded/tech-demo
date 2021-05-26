cff972d39a70f80e9e5d97fc1b1fdc2fe6573c52
ma9yam <ma9yam@gmail.com>
2021-05-27 05:07:33 +1000

    US 58: end scene added

---
+++ Tech demo/Assets/Scripts/screenflash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenflash : MonoBehaviour
+  
+{
+    public Animator flashanim;
+    IEnumerator OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            flashanim.SetBool("flashing", true);
+            yield return new WaitForSeconds(1);
+            flashanim.SetBool("flashing", false);
+            Destroy(gameObject);
+
+        }
+    }
+ 
+}
---
+++ Tech demo/Assets/endscreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endscreen : MonoBehaviour
+{
+    public Animator endanim;
+  
+    IEnumerator OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            endanim.SetBool("end", true);
+            yield return new WaitForSeconds(15);
+          
+            SceneManager.LoadSceneAsync(0);
+
+        }
+    }
+
+}
